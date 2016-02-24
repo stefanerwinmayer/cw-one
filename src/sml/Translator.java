1f1e8c9d32d98855fe437af3ccb1dc3d63caa30f
Stefan E. Mayer <stefan.e.mayer@gmail.com>
2016-02-24 13:56:55 +0000

    Implemented translation for instruction 'sub'.

--- src/sml/Translator.java
+++ src/sml/Translator.java
@@ -92,6 +92,11 @@ public class Translator {
                 r = scanInt();
                 s1 = scanInt();
                 return new LinInstruction(label, r, s1);
+            case "sub":
+                r = scanInt();
+                s1 = scanInt();
+                s2 = scanInt();
+                return new SubInstruction(label, r, s1, s2);
         }
 
         // You will have to write code here for the other instructions.
