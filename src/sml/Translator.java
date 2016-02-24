b451f0e789eae76b3f7ee65e171fc19a54491444
Stefan E. Mayer <stefan.e.mayer@gmail.com>
2016-02-24 14:59:27 +0000

    Implemented translation for out instruction.

--- src/sml/Translator.java
+++ src/sml/Translator.java
@@ -107,6 +107,9 @@ public class Translator {
                 s1 = scanInt();
                 s2 = scanInt();
                 return new DivInstruction(label, r, s1, s2);
+            case "out":
+                r = scanInt();
+                return new OutInstruction(label, r);
         }
 
         // You will have to write code here for the other instructions.
