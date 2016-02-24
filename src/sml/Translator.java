3371ace5bd112e56026eba032fbfd30c9982d975
Stefan E. Mayer <stefan.e.mayer@gmail.com>
2016-02-24 14:26:58 +0000

    Implemented translation for div instruction.

--- src/sml/Translator.java
+++ src/sml/Translator.java
@@ -102,6 +102,11 @@ public class Translator {
                 s1 = scanInt();
                 s2 = scanInt();
                 return new MulInstruction(label, r, s1, s2);
+            case "div":
+                r = scanInt();
+                s1 = scanInt();
+                s2 = scanInt();
+                return new DivInstruction(label, r, s1, s2);
         }
 
         // You will have to write code here for the other instructions.
